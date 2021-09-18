54bb080e7133efb426706e8cbfdde5b3b37efdab
Ваня Рыбак <infinity.man102@gmail.com>
2021-09-18 11:38:59 +0300

    Homework 1

---
+++ Homework1/Homework1.py
@@ -0,0 +1,33 @@
+print("Exercise 1:")
+print("Hello, world!")
+
+print("Exercise 2:")
+print("My name is Ivan.", "I am 18.", "I am studing in NAU.")
+
+print("Exercise 3:")
+a = float(input("Input length of a rectangle: "))
+b = float(input("Input width of a rectangle: "))
+area = a*b
+print("Area of the rectangle: ", area)
+
+print("Exercise 4:")
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+sum = x + y
+print("Sum of two number is ", sum)
+product = x * y
+print("Product of two number is ", product)
+dif = x - y
+print("Difference of two number is ", dif)
+quot = x / y
+print("Quotient of two number is ", quot)
+
+print("Exercise 5:")
+p = 3.14159
+r = float(input("Enter radius of the circle: "))
+d = 2 * r
+print("Diameter of the circle is ", d)
+c = 2 * p * r
+print("Circumference of the circle is ", c)
+s = p * r ** 2
+print("Area of the circle is ", s)
